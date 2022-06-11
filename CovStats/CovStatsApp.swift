7b7abba21b3e0858d3dcce2d5b129f80541c8d24
dekapd99 <dekaprimatio@gmail.com>
2022-06-11 21:04:41 +0700

    Initial Commit

---
+++ CovStats/CovStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovStatsApp.swift
+//  CovStats
+//
+//  Created by Deka Primatio on 11/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CovStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
